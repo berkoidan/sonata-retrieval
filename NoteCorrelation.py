3d5f800b2f01e369ea9f2d0d1c344d1cee75d08a
Idan Berkovits <berko@cymphony.io>
2024-07-27 22:48:17 +0300

    moved iteration to separate function

--- NoteCorrelation.py
+++ NoteCorrelation.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Callable
+from typing import Any, Callable, Iterator
 
 import numpy as np
 from tis.TIS import TIS
@@ -7,19 +7,20 @@ from tis.NoteCluster import NoteCluster, sum_clusters
 
 logger = logging.getLogger(__name__)
 
+def cluster_windows(clusters:list[NoteCluster], windowSize:int) -> Iterator[NoteCluster]:
+    window = sum_clusters(clusters[:windowSize])    
+    for i in range(windowSize, len(clusters)):
+        yield window
+        window += clusters[i]
+        window -= clusters[i - windowSize]
+
+
 def correlation(right:NoteCluster, 
                 clusters:list[NoteCluster], 
                 metric: Callable[[NoteCluster, NoteCluster], np.floating[Any]], 
-                windowSize:int=1) -> None:
-    left:NoteCluster = sum_clusters(clusters[:windowSize])
-    for i in range(windowSize, len(clusters) + 1):
+                windowSize:int=1) -> None:    
+    for left in cluster_windows(clusters, windowSize):    
         if len(left) == 0:
             continue
 
         logger.info(f"{right} ({round(TIS.norm(right))}) <-> {left} ({round(TIS.norm(left))}):\t{metric(right, left)}")
-
-        if i >= len(clusters):
-            continue
-        
-        left += clusters[i]
-        left -= clusters[i - windowSize]
